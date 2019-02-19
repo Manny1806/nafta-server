431ad2d056e9472e460864493987ed0b0a0c35ff
Alex <alex@localhost.localdomain>
2019-02-18 19:46:20 -0800

    feedback router

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ module.exports = {
     PORT: process.env.PORT || 8080,
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://localhost:3000',
     MONGODB_URI: "mongodb://alex:r5t6y7u8@ds229701.mlab.com:29701/nafta" || 'mongodb://localhost/nafta',
-    JWT_SECRET: "MEOW",
-    JWT_EXPIRY: '7d'
+    JWT_SECRET: process.env.SECRET,
+    JWT_EXPIRY: '7d',
+    GMAIL_USER: process.env.GMAILUSER,
+    GMAIL_PASSWORD: process.env.GMAILPASSWORD
 }
